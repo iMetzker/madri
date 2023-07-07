34536fbd57dd0d36a52c22af1972c88526a22b65
Ivny Metzker <ivnymetzker@gmail.com>
2023-07-07 18:17:44 -0300

    Teste Logo

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+/* MENU */
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu () {
+    const nav = document.querySelector('#nav');
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+/* ANIMATION */

08a76c39d819c2666f9a16edb38f6c6290adcf04
Metzker <ivnymetzker@gmail.com>
2023-09-29 22:04:59 -0300

    adding menu style

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,26 +5,40 @@ const btnMobile = document.getElementById('btn-mobile');
 function toggleMenu () {
     const nav = document.querySelector('#nav');
     nav.classList.toggle('active');
+
 }
 
 btnMobile.addEventListener('click', toggleMenu);
 
+/* fechando o menu ao clique */
+
+const ul = document.querySelector('#nav ul');
+const ulElements = ul.querySelectorAll('li');
+
+ulElements.forEach((element) => {
+    element.addEventListener('click', () => {
+        toggleMenu(); 
+    });
+});
+
 /* ANIMATION */
 
 const targets = document.querySelectorAll('[data-animated]');
+const animationClass = 'animate';
 
 function animateElements() {
-  const animationClass = 'animate';
-  const windowTop = window.pageYOffset + window.innerHeight * 0.75;
+  const windowTop = window.scrollY + window.innerHeight * 0.75;
 
   targets.forEach((element) => {
-    if (windowTop > element.offsetTop) {
+    const elementTop = element.getBoundingClientRect().top + window.scrollY;
+
+    if (windowTop > elementTop) {
       element.classList.add(animationClass);
-    } else {
-      element.classList.remove(animationClass);
     }
   });
 }
+
+animateElements();
 
 if (targets.length > 0) {
   window.addEventListener('scroll', animateElements);

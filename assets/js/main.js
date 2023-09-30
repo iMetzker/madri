1275cf74170fadac7b5f36225cf7c9ee674009e0
Metzker <ivnymetzker@gmail.com>
2023-09-29 21:40:05 -0300

    adding scroll effects

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,22 @@ function toggleMenu () {
 btnMobile.addEventListener('click', toggleMenu);
 
 /* ANIMATION */
+
+const targets = document.querySelectorAll('[data-animated]');
+
+function animateElements() {
+  const animationClass = 'animate';
+  const windowTop = window.pageYOffset + window.innerHeight * 0.75;
+
+  targets.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  });
+}
+
+if (targets.length > 0) {
+  window.addEventListener('scroll', animateElements);
+}
